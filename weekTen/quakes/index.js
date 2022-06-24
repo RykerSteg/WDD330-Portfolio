817a2a282c6a151a79b6bd5f442ceab5812c3c0a
RykerSteg <rykersteglich@gmail.com>
2022-06-23 22:26:40 -0600

    quakes

---
+++ weekTen/quakes/index.js
@@ -0,0 +1,16 @@
+                // function testGetQuakesForLocation() {
+                //     // call the getLocation function to get the lat/long
+                //   getLocation();
+                //    // use that information to build out the correct URL
+                //     const geoUrl = baseUrl + // add location information here
+                //     // use the url to request the correct quakes 
+                
+                //     //log out the quakes for now.
+                //     console.log(geoUrl);
+                //   }
+                  //getQuakesForLocation();
+
+import QuakesController from './QuakesController.js';
+
+const myQuakesController = new QuakesController();
+myQuakesController.init();
